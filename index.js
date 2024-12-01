e426dd306c7049d2c5360568928dfc493d96d8ac
danish-ibrahim-khan <143989917+danish-ibrahim-khan@users.noreply.github.com>
2024-12-01 23:20:53 +0530

    ddd

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ import express from "express";
 
 const app = express();
 const port = 9000;
-app.use("/",(req,res)=>{
-    res.json({message: "hello world"})
-})
 
 app.listen(9000, ()=> {
     console.log(`server running on ${port}`)
