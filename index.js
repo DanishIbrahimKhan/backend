8a5e76044390f471d096e12b1dac3a060c1bebbd
danish-ibrahim-khan <143989917+danish-ibrahim-khan@users.noreply.github.com>
2024-12-01 23:17:39 +0530

    api

--- index.js
+++ index.js
@@ -14,23 +14,6 @@ app.listen(9000, ()=> {
 const OMDB_API_KEY = '3aa28eff';
 const OMDB_BASE_URL = 'http://www.omdbapi.com/';
 
-// Serve HTML content on the root path
-app.get('/', (req, res) => {
-  res.send(`
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="utf-8">
-      <title>Serverless Movie API</title>
-    </head>
-    <body>
-      <h1>Welcome to the Serverless Movie API</h1>
-      <p>Use the endpoint <code>/movie-by-title/{movie-name}</code> to search for a movie.</p>
-    </body>
-    </html>
-  `);
-});
-
 app.get('/movie-by-title/:t', async (req, res) => {
   const { t } = req.params;
   try {
