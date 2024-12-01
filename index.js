4468cce2eed5aeab1cbbc800465c11a18d0209bc
danish-ibrahim-khan <143989917+danish-ibrahim-khan@users.noreply.github.com>
2024-12-01 23:46:02 +0530

    push

--- index.js
+++ index.js
@@ -12,8 +12,6 @@ app.use(cors()); // Enable cross-origin requests
 
 app.get("/movie-by-title/:t", async (req, res) => {
   const { t } = req.params;
-  console.log(`Fetching movie with title: ${t}`); // Debugging
-
   try {
     const response = await axios.get(`${OMDB_BASE_URL}?t=${encodeURIComponent(t)}&apikey=${OMDB_API_KEY}`);
     console.log("OMDB API Response:", response.data); // Debugging
@@ -24,10 +22,26 @@ app.get("/movie-by-title/:t", async (req, res) => {
       res.status(404).json({ error: response.data.Error });
     }
   } catch (error) {
-    console.error("Error fetching movie:", error.message || error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
+
+
+app.get("/movie-by-id/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+      const response = await axios.get(`${OMDB_BASE_URL}?id=${encodeURIComponent(t)}&apikey=${OMDB_API_KEY}`);
+      console.log("OMDB API Response:", response.data); // Debugging
+  
+      if (response.data.Response === "True") {
+        res.json(response.data);
+      } else {
+        res.status(404).json({ error: response.data.Error });
+      }
+    } catch (error) {
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  });
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
